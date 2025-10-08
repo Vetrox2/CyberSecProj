3839a5a4816395fe459bf5dada93fa11a5f9aad1
Vetrox2 <mateuszmulka@spoko.pl>
2025-10-08 18:49:40 +0200

    chore: init migration and register services to DI

---
+++ backend/Migrations/20251007195531_Init.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    MustChangePassword = table.Column<bool>(type: "bit", nullable: false),
+                    IsBlocked = table.Column<bool>(type: "bit", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordValidTo = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    RequirePasswordRules = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ backend/Services/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .HasIndex(u => u.Login)
+                .IsUnique();
+
+            builder.Entity<User>()
+                .Property(u => u.Login)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Entity<User>()
+                .Property(u => u.PasswordHash)
+                .IsRequired();
+        }
+    }
+}
